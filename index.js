037bee4d8468a84570e0b40e53832df6c0110f0b
Jerricke <eejerrickw@gmail.com>
2023-06-12 11:20:03 -0600

    complete

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+
+// function multiply(num1, num2) {
+//     const res = (num1*num2)
+//     return res;
+// }
+// multiply(31,2)
+const num1 = 1;
+const num2 = 62;
+const multiply = num1 * num2;
+
+// function random() {
+//     const someInt = Number.parseInt(((Math.random() * 10) + 1 ), 10);
+//     return someInt;
+// }
+
+const random = Number.parseInt(Math.random() + 1)
+
+const num3 = 9;
+const num4 = 5;
+
+const mod = num3 % num4; 
+
+const max = Math.max(1,4,12,20);
